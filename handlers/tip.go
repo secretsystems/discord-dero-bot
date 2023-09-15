37539eb0bc78ebcbb401d0cd03eab10b03ef8dbe
secretnamebasis <secretnamebasis@keemail.me>
2023-09-15 06:53:23 -0600

    feature: !tip_registered , MakeBulkTransfers

--- handlers/tip.go
+++ handlers/tip.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"regexp"
 	"strings"
 
@@ -32,6 +33,46 @@ func HandleTip(session *discordgo.Session, message *discordgo.MessageCreate) {
 		return
 	}
 
+	if content == "!tip_registered" {
+		// Load user mappings from the JSON file
+		loadUserMappings()
+
+		// Create an array to store transfer information
+		var transfers []dero.TransferInfo
+		var discordIDs []string // To store Discord user IDs involved in transfers
+
+		// Iterate through user mappings and create TransferInfo objects
+		for discordID, address := range userMappings {
+			address = resolveWalletAddress(address)
+			transfer := dero.TransferInfo{
+				Destination: address,
+				Amount:      200, // Set the desired tip amount
+			}
+			transfers = append(transfers, transfer)
+
+			// Add the Discord user ID to the list of users involved
+			discordIDs = append(discordIDs, discordID)
+
+			// If we have 8 transfers, perform the bulk transfer and reset the transfers slice
+			if len(transfers) == 8 {
+				log.Printf("Before processing transfers: %v", transfers)
+				processTransfers(session, message, transfers, discordIDs)
+				log.Printf("After processing transfers")
+				transfers = nil
+				discordIDs = nil
+			}
+		}
+
+		// Process any remaining transfers
+		if len(transfers) > 0 {
+			log.Printf("Before processing transfers: %v", transfers)
+			processTransfers(session, message, transfers, discordIDs)
+			log.Printf("After processing transfers")
+		}
+
+		return
+	}
+
 	if strings.HasPrefix(content, "!tip ") {
 		input := strings.TrimPrefix(content, "!tip ")
 
@@ -43,7 +84,7 @@ func HandleTip(session *discordgo.Session, message *discordgo.MessageCreate) {
 		}
 
 		// Resolve wallet address or name
-		recipientAddress := resolveWalletAddress(input, message.Author.ID)
+		recipientAddress := resolveWalletAddress(input)
 
 		if recipientAddress == "" {
 			session.ChannelMessageSend(message.ChannelID, "Invalid address or wallet name. Please consider using `/register`")
@@ -58,6 +99,51 @@ func HandleTip(session *discordgo.Session, message *discordgo.MessageCreate) {
 
 		// Send the tip
 		handleTip(session, message, recipientAddress)
+	}
+}
+
+func processTransfers(session *discordgo.Session, message *discordgo.MessageCreate, transfers []dero.TransferInfo, discordIDs []string) {
+	// Create a channel to communicate the result of the bulk transfer
+	resultChan := make(chan string)
+	defer close(resultChan) // Close the channel when done
+
+	// Use a goroutine to make the bulk transfer concurrently
+	go func() {
+		// Call MakeBulkTransfer to perform the bulk transfer
+		txID, err := dero.MakeBulkTransfer(transfers)
+		if err != nil {
+			resultChan <- "Error sending bulk tip: " + err.Error()
+		} else {
+			resultChan <- txID // Send the transaction ID to the channel
+		}
+	}()
+
+	// Display the txid along with the success message and mention Discord users by their IDs
+	messageToSend := "Bulk tip status:\n```"
+	txIDReceived := false
+
+	// Wait for the result from the goroutine
+	select {
+	case result := <-resultChan:
+		if strings.HasPrefix(result, "Error") {
+			messageToSend += result
+		} else {
+			messageToSend += fmt.Sprintf("TxID: %s```\nExplore this transaction by visiting http://explorer.dero.io/tx/%s \nTips went to these registered badasses:\n", result, result)
+			txIDReceived = true
+		}
+	}
+
+	for _, discordID := range discordIDs {
+		messageToSend += fmt.Sprintf("- <@%s>\n", discordID) // Mention users using Discord IDs
+	}
+
+	// Optionally, you can perform additional actions based on whether the bulk transfer was successful or not.
+	if txIDReceived {
+		// Send the message to the Discord channel
+		session.ChannelMessageSend(message.ChannelID, messageToSend)
+	} else {
+		messageToSend = "TX FAILED TO BUILD TRANSFER"
+		session.ChannelMessageSend(message.ChannelID, messageToSend) // Handle the case where the bulk transfer failed.
 	}
 }
 
@@ -83,18 +169,17 @@ func handleMention(session *discordgo.Session, message *discordgo.MessageCreate,
 	handleTip(session, message, mappedAddress)
 }
 
-func resolveWalletAddress(input, userID string) string {
+func resolveWalletAddress(input string) string {
 	if len(input) == 66 && strings.HasPrefix(input, "dero") {
 		return input
 	}
 
-	mappedAddress := getUserAddress(userID)
 	if addr, ok := userMappings[input]; ok && len(addr) == 66 && strings.HasPrefix(addr, "dero") {
 		return addr
 	}
 
 	lookupResult, err := dero.WalletNameToAddress(input) // Implement the wallet name lookup function
-	if err != nil || lookupResult == "" || lookupResult == mappedAddress {
+	if err != nil || lookupResult == "" {
 		return ""
 	}
 
@@ -116,13 +201,13 @@ func getUserAddress(userID string) string {
 	return userMappings[userID]
 }
 
-func handleTip(session *discordgo.Session, message *discordgo.MessageCreate, recipientAddress string) {
+func handleUserPermissions(session *discordgo.Session, message *discordgo.MessageCreate) (amnt int, amntmsg string) {
 	// Get the user's roles
 	userRoles := message.Member.Roles
 
 	// Define default tip amount and message
-	amnt := 2
-	amntmsg := "0.00002 DERO, or 2 DERI"
+	amnt = 2
+	amntmsg = "0.00002 DERO, or 2 DERI"
 
 	// Check user roles and adjust tip amount based on role priority
 	for _, roleID := range userRoles {
@@ -142,7 +227,10 @@ func handleTip(session *discordgo.Session, message *discordgo.MessageCreate, rec
 			}
 		}
 	}
-
+	return amnt, amntmsg
+}
+func handleTip(session *discordgo.Session, message *discordgo.MessageCreate, recipientAddress string) {
+	amnt, amntmsg := handleUserPermissions(session, message)
 	// Rest of the function remains the same as in the previous examples
 	session.ChannelMessageSend(message.ChannelID, fmt.Sprintf("`secret-wallet` is sending %s\nThis process takes roughly 18 seconds; or 1 block interval.", amntmsg))
 
---
+++ utils/dero/make_bulk_transfer.go
@@ -0,0 +1,77 @@
+package dero
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func MakeBulkTransfer(transfers []TransferInfo) (string, error) {
+	// Define payload data
+	payloadData := map[string]interface{}{
+		"jsonrpc": "2.0",
+		"id":      "1",
+		"method":  "transfer",
+		"params": map[string]interface{}{
+			"ringsize":  8,
+			"transfers": transfers,
+		},
+	}
+
+	// Marshal payload data to JSON
+	payloadJSON, err := json.Marshal(payloadData)
+	if err != nil {
+		return "", fmt.Errorf("Error marshaling JSON: %v", err)
+	}
+
+	// Define HTTP client and request
+	client := &http.Client{}
+	url := fmt.Sprintf("http://%s:%s/json_rpc", deroServerIP, deroWalletPort)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadJSON))
+	if err != nil {
+		return "", fmt.Errorf("Error creating request: %v", err)
+	}
+	req.SetBasicAuth(deroUser, deroPass)
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send HTTP request
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Error sending request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read and process response body
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("Error reading response body: %v", err)
+	}
+
+	// Print response body
+	fmt.Printf("Response Body: %s\n", responseBody)
+
+	// Parse response JSON
+	var response map[string]interface{}
+	err = json.Unmarshal(responseBody, &response)
+	if err != nil {
+		return "", fmt.Errorf("Error decoding response JSON: %v", err)
+	}
+
+	if responseError, ok := response["error"].(map[string]interface{}); ok {
+		return "", fmt.Errorf("DERO wallet error: %v", responseError)
+	}
+
+	if txID, ok := response["result"].(map[string]interface{})["txid"].(string); ok {
+		return txID, nil
+	}
+
+	return "", fmt.Errorf("No transaction ID found in the response")
+}
+
+type TransferInfo struct {
+	Destination string `json:"destination"`
+	Amount      int    `json:"amount"`
+}
