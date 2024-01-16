861b4f6df6bbdf30232fe48db2af7e21112f1d3a
secretnamebasis <secretnamebasis@keemail.me>
2024-01-15 19:05:58 -0700

    minor: include grok image

--- handlers/grokked.go
+++ handlers/grokked.go
@@ -1,11 +1,37 @@
 package handlers
 
 import (
+	"log"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/secretsystems/discord-dero-bot/utils/dero"
 )
 
 // Handle !grok command
 func HandleGrok(session *discordgo.Session, message *discordgo.MessageCreate) {
-	session.ChannelMessageSend(message.ChannelID, dero.GetGrok())
+	imagePath := "assets/oogaboogaaa.jpg"
+	imageFile, err := os.Open(imagePath)
+	if err != nil {
+		log.Println("Error opening image path:", err)
+		return
+	}
+	defer imageFile.Close()
+
+	image := &discordgo.File{
+		Name:        "oogaboogaaa.jpg",
+		ContentType: "image/png",
+		Reader:      imageFile,
+	}
+	content := dero.GetGrok()
+
+	messageSend := &discordgo.MessageSend{
+		Files:   []*discordgo.File{image},
+		Content: content,
+	}
+
+	_, err = session.ChannelMessageSendComplex(message.ChannelID, messageSend)
+	if err != nil {
+		log.Println("Error sending grokked info:", err)
+	}
 }
